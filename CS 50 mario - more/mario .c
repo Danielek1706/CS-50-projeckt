12676e5a3773e9f646a84371ede40c249a3f662e
Danielek <148774878+Danielek1706@users.noreply.github.com>
2023-12-29 11:21:58 +0100

    Add files via upload

---
+++ CS 50 mario - more/mario .c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+
+int height, row, column, space ;   // height to wysokosc, row - wiersz, column - kolumna, space - przestrzen
+
+do
+{
+
+height = get_int ("Enter height here: ");
+}
+while (height < 1 || height > 8);
+
+for(row = 0; row < height; row++)
+
+{
+
+  for (space = 0; space < height - row - 1; space++) // zmiana kierunku na lewa strone
+
+    {
+        printf (" ");
+    }
+
+    for ( column = 0; column <= row; column++)
+
+{
+    printf("#");
+}
+    printf("  ");
+
+    for (column = 0; column <=row; column++) // dopisac kod z ostatniego projektu
+
+   {
+     printf ("#");
+
+   }
+
+    printf("\n");
+}
+
+}
+
+
